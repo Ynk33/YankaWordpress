95eb3d7fe6d335470b55febd6f24e63f840596ef
Yannick Tirand <yannick.tirand@gravitygoat.com>
2024-04-06 11:13:33 +1300

    Adds safe check in case the Docker container is not running

--- setup.sh
+++ setup.sh
@@ -16,6 +16,18 @@ DB_CONTAINER=${DIR,,}-db-1
 
 IP_ADDRESS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $DB_CONTAINER)
 
+if [ -z "${IP_ADDRESS}" ]
+then
+  echo
+  echo -e "\033[31mCouldn't retrieve the database container IP address!\033[0m"
+  echo This is more likely because the Docker container is not running.
+  echo
+  echo -e "\033[33mPlease, ensure the Docker container is running (\033[0mdocker compose up\033[33m) and launch this script again.\033[0m"
+  echo
+
+  exit 126;
+fi
+
 echo -e "- Wordpress container: \t\t\t\033[33m$WP_CONTAINER \033[0m"
 echo -e "- Database container: \t\t\t\033[33m$DB_CONTAINER \033[0m"
 echo -e "- Database container IP address: \t\033[33m$IP_ADDRESS \033[0m"
@@ -35,6 +47,13 @@ echo Executing db script on Docker container...
 docker exec $WP_CONTAINER bash -c "bash < ./db.sh"
 
 echo -e "\033[32mDone.\033[0m"
+echo
+
+echo Reverting db script...
+
+sed -i -E "s|(IP_ADDRESS=).+|\1MY_IP|g" db.sh
+
+echo -e "\033[32mDone. \033[0m"
 echo
 
 echo -e "\033[32mYour \033[0m$DIR\033[32m project is all set up. Have fun! \033[0m"
