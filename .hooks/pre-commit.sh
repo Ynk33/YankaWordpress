73dd8ad1dfa2893195303fb1db241e8ac45ec943
Yannick Tirand <yannick.tirand@gravitygoat.com>
2024-04-06 11:46:51 +1300

    Test pre-commit hook

---
+++ .hooks/pre-commit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#!/bin/bash
+
+echo Test pre-commit hook
--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ echo "########### YANKAWORDPRESS SETUP SCRIPT #############"
 echo "#####################################################"
 echo
 
+# Retrieving data
 echo Retrieving data...
 
 DIR=${PWD##*/}
@@ -16,6 +17,7 @@ DB_CONTAINER=${DIR,,}-db-1
 
 IP_ADDRESS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $DB_CONTAINER)
 
+# Check if the Docker container is running, and stop the script if not, because otherwise the db cannot be dumped
 if [ -z "${IP_ADDRESS}" ]
 then
   echo
@@ -35,6 +37,7 @@ echo -e "- Database container IP address: \t\033[33m$IP_ADDRESS \033[0m"
 echo -e "\033[32mAll data ready! \033[0m"
 echo
 
+# Update the db.sh script to insert the Docker database container IP address
 echo Updating db script...
 
 sed -i -E "s|(IP_ADDRESS=).+|\1$IP_ADDRESS|g" db.sh
@@ -42,6 +45,7 @@ sed -i -E "s|(IP_ADDRESS=).+|\1$IP_ADDRESS|g" db.sh
 echo -e "\033[32mDone. \033[0m"
 echo
 
+# Dump the database from the Docker container
 echo Executing db script on Docker container...
 
 docker exec $WP_CONTAINER bash -c "bash < ./db.sh"
@@ -49,12 +53,23 @@ docker exec $WP_CONTAINER bash -c "bash < ./db.sh"
 echo -e "\033[32mDone.\033[0m"
 echo
 
+# Revert the db.sh script, to leave no trace
 echo Reverting db script...
 
 sed -i -E "s|(IP_ADDRESS=).+|\1MY_IP|g" db.sh
 
 echo -e "\033[32mDone. \033[0m"
 echo
+
+# Setup the git-hooks folder
+echo Setting up Git hooks...
+
+git config core.hookPath .hooks
+
+echo -e "\033[32mDone. \033[0m"
+echo
+
+# All good!
 
 echo -e "\033[32mYour \033[0m$DIR\033[32m project is all set up. Have fun! \033[0m"
 echo
