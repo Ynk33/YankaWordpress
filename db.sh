2b584fe4e31cb7f15f5c4c31f8c207f6409f5e1e
Yannick Tirand <yannick.tirand@gravitygoat.com>
2024-04-05 13:37:43 +1300

    Updates db to clean some remnants of old data

--- db.sh
+++ db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-IP_ADDRESS=192.168.32.2
+IP_ADDRESS=MY_IP
 
 mysql -h $IP_ADDRESS -u wordpress -pwordpress -Nse 'show tables' wordpress | while read table; do mysql -h $IP_ADDRESS -u wordpress -pwordpress -e "TRUNCATE TABLE $table" wordpress; done
 mysql -h $IP_ADDRESS -u wordpress -pwordpress wordpress < dump_full.sql
