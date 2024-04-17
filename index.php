dd20371956493c84cee20166a2773a94e6292d6e
Yannick Tirand <ytirand@gmail.com>
2024-04-17 16:50:11 +1200

    Revert changes because actually wordpress already does that

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 
-
-/**
- * Redirect request to home to the real front.
- */
-
-$uri = $_SERVER['REQUEST_URI'];
-$env = parse_ini_file(".env");
-if ($uri == '/' && !empty($env) && !empty($env['FRONT_URL'])) {
-  header('Location: ' . $env['FRONT_URL'], true, 301);
-  die();
-}
-
-
-
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
