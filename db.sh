87bb4e339a6ea3661715e187928be0218657fbe3
Yannick Tirand <ytirand@gmail.com>
2024-05-06 17:25:35 +1200

    Removes all dumps and dumpings, as they are now done with a specific command

--- db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-IP_ADDRESS=MY_IP
-
-mysql -h $IP_ADDRESS -u wordpress -pwordpress -Nse 'show tables' wordpress | while read table; do mysql -h $IP_ADDRESS -u wordpress -pwordpress -e "TRUNCATE TABLE $table" wordpress; done
-mysql -h $IP_ADDRESS -u wordpress -pwordpress wordpress < dump_full.sql
